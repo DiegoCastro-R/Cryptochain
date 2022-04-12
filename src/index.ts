a6d7ec0c2a9abd82b2d6f83e4563eb829ab83880
Diego C. Rodrigues <diego.rodrigues@growtisolutions.com>
2022-04-12 20:15:11 -0300

    feat: setup post request to mine a block

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import express, { Request, Response } from 'express';
 
 import { Blockchain } from "./blockchain";
 
+//* Create a new express application instance
 const app = express()
+app.use(express.json())
+
 const blockchain = new Blockchain();
 
 app.get('/api/blocks', (req: Request, res: Response) => {
     res.json(blockchain.chain);
+})
+
+app.post('/api/mine', (req: Request, res: Response) => {
+    const block = blockchain.addBlock({ data: req.body.data });
+
+    res.redirect('/api/blocks');
 })
 
 const PORT = 3000
